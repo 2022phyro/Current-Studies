fbaf2f341d0b1c30fa3b1f656a1fd563dbc85057
Ugwuanyi Afam <111662034+2022phyro@users.noreply.github.com>
2024-01-28 11:08:20 +0000

    Finished challenge

--- 0x00-server-side-javascript-with-node/week-7/calculator-app-boilerplate-master/test/calculator.spec.js
+++ 0x00-server-side-javascript-with-node/week-7/calculator-app-boilerplate-master/test/calculator.spec.js
@@ -16,12 +16,17 @@ describe('Calculator testing', function () {
     describe('Addition functionality testing', function () {
       // testcase to test is dependencies are used or not
       it('Add two positive numbers, returning get positive sum', function () {
-        // Write the testing logic here
+        assert.equal(calculator('a', {lhs: 1, rhs:2}), 3);
+        assert.equal(calculator('a', {lhs: 50, rhs:2}), 52);
+        assert.equal(calculator('a', {lhs: 1.2, rhs:2}), 3.2);
       });
       // test case to test add functionality
 
       it('Add two negative numbers, returning get negative sum', function () {
         // Write the testing logic here
+        assert.equal(calculator('a', {lhs: -1, rhs:-2}), -3);
+        assert.equal(calculator('a', {lhs: -50, rhs: -2}), -52);
+        assert.equal(calculator('a', {lhs: -1.2, rhs:-2}), -3.2);
       });
 
       // test case to test add functionality
@@ -29,12 +34,17 @@ describe('Calculator testing', function () {
         , function () {
 
           // Write the testing logic here
-
+          assert.equal(calculator('a', {lhs: -1, rhs:2}), 1);
+          assert.equal(calculator('a', {lhs: 50, rhs:-2}), 48);
+          assert.equal(calculator('a', {lhs: -1.2, rhs:2}), 0.8);
         });
       // test case to test add functionality
       it('Add zeros, produces zero', function () {
 
         // Write the testing logic here
+        assert.equal(calculator('a', {lhs: 1, rhs:-1}), 0);
+        assert.equal(calculator('a', {lhs: 50, rhs:0}), 50);
+        assert.equal(calculator('a', {lhs: 0, rhs:-1.2}), -1.2);
       });
     });
     describe('Subtraction functionality testing', function () {
@@ -42,44 +52,62 @@ describe('Calculator testing', function () {
       it('Subtract two positive numbers, returning get positive subtraction',
         function () {
           // Write the testing logic here
+          assert.equal(calculator('s', {lhs: 5, rhs: 3}), 2);
+          assert.equal(calculator('s', {lhs: 10, rhs: 5}), 5);
         });
 
       // test case to test subtract functionality
       it('Subtract two negative numbers, returning get negative subtraction',
         function () {
           // Write the testing logic here
+          assert.equal(calculator('s', {lhs: -5, rhs: -3}), -2);
+          assert.equal(calculator('s', {lhs: -10, rhs: -5}), -5);
+
         });
 
       // test case to test subtract functionality
       it('Subtract two number, with either of them is negative, producing sum output'
         , function () {
           // Write the testing logic here
+          assert.equal(calculator('s', {lhs: 5, rhs: -3}), 8);
+          assert.equal(calculator('s', {lhs: -5, rhs: 3}), -8);
         });
 
       // test case to test subtract functionality
       it('Subtract zeros, produces zero', function () {
         // Write the testing logic here
+        assert.equal(calculator('s', {lhs: 0, rhs: 0}), 0);
+
       });
     });
     describe('Multiplication functionality testing', function () {
       // test case to test multiply functionality
       it('Multiply two positive numbers, returning get positive Multiplication', function () {
         // Write the testing logic here
+        assert.equal(calculator('m', {lhs: 5, rhs: 3}), 15);
+        assert.equal(calculator('m', {lhs: 10, rhs: 5}), 50);
       });
       // test case to test multiply functionality
       it('Multiply two negative numbers, returning get positive Multiplication', function () {
         // Write the testing logic here
+        assert.equal(calculator('m', {lhs: -5, rhs: -3}), 15);
+        assert.equal(calculator('m', {lhs: -10, rhs: -5}), 50);
       });
       // test case to test multiply functionality
       it(`Multiply two number, with either of them is negative,
         producing negative multiplication output`,
         function () {
           // Write the testing logic here
+          assert.equal(calculator('m', {lhs: 5, rhs: -3}), -15);
+          assert.equal(calculator('m', {lhs: -5, rhs: 3}), -15);
         });
 
       // test case to test multiply functionality
       it('Multiply zeros, produces zero', function () {
         // Write the testing logic here
+        assert.equal(calculator('m', {lhs: 0, rhs: 0}), 0);
+        assert.equal(calculator('m', {lhs: 0, rhs: 5}), 0);
+        assert.equal(calculator('m', {lhs: 5, rhs: 0}), 0);
       });
     });
     describe('Division functionality testing', function () {
@@ -87,12 +115,17 @@ describe('Calculator testing', function () {
 
       it('Divide two positive numbers, returning get positive Multiplication', function () {
         // Write the testing logic here
+        assert.equal(calculator('d', {lhs: 10, rhs: 5}), 2);
+        assert.equal(calculator('d', {lhs: 15, rhs: 3}), 5);
       });
 
 
       // test case to test divide functionality
       it('Divide two negative numbers, returning get positive Multiplication', function () {
         // Write the testing logic here
+        assert.equal(calculator('d', {lhs: -10, rhs: -5}), 2);
+        assert.equal(calculator('d', {lhs: -15, rhs: -3}), 5);
+
       });
 
       // test case to test divide functionality
@@ -100,11 +133,16 @@ describe('Calculator testing', function () {
       it('Divide two number, with either of them is negative, producing negative Division output',
         function () {
           // Write the testing logic here
+          assert.equal(calculator('d', {lhs: 10, rhs: -5}), -2);
+          assert.equal(calculator('d', {lhs: -15, rhs: 3}), -5);
+
         });
 
       // test case to test divide functionality
       it(`Should not divide by 0, producing 'Can not divide by zero' message`, function () {
         // Write the testing logic here
+        assert.equal(calculator('d', {lhs: 10, rhs: 0}), 'Can not divide by zero');
+
       });
     });
     describe('Unknown operation testing', function () {
@@ -113,6 +151,10 @@ describe('Calculator testing', function () {
         producing 'Unknown operation' message`,
         function () {
           // Write the testing logic here
+          assert.equal(calculator('x', {lhs: 10, rhs: 5}), 'Unknown operation');
+          assert.equal(calculator('Y', {lhs: 10, rhs: 5}), 'Unknown operation');
+          assert.equal(calculator('/', {lhs: 10, rhs: 5}), 'Unknown operation');
+
         });
     });
   });
